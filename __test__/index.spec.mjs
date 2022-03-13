a9191579228f475741c2811c52b5e19ac2576805
LongYinan <lynweklm@gmail.com>
2022-03-14 01:29:05 +0800

    test: tweak logic

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -9,17 +9,18 @@ const __dirname = join(fileURLToPath(import.meta.url), '..')
 import { getFileLatestModifiedDateByGit } from '../index.js'
 
 test('Date should be equal with cli', (t) => {
+  const workDir = join(__dirname, '..')
   let date
   try {
-    date = execSync('git log -1 --format=%cd --date=iso src/lib.rs', {
-      cwd: join(__dirname, '..'),
+    date = execSync('git log -1 --format=%cd --date=iso package.json', {
+      cwd: workDir,
     }).toString('utf8')
   } catch (e) {
-    t.pass('Git not installed')
+    t.notThrows(() => getFileLatestModifiedDateByGit(workDir, 'package.json'))
     return
   }
   t.is(
     new Date(date).valueOf(),
-    getFileLatestModifiedDateByGit(join(__dirname, '..'), join('src', 'lib.rs'))
+    getFileLatestModifiedDateByGit(workDir, 'package.json')
   )
 })
