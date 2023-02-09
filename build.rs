840edb3fc948e6add367657602240eb69873f04f
LongYinan <lynweklm@gmail.com>
2023-02-09 11:34:33 +0800

    chore: upgrade toolchain (#11)

--- build.rs
+++ build.rs
@@ -4,9 +4,6 @@ fn main() {
   napi_build::setup();
   let compile_target = std::env::var("TARGET").unwrap();
   if compile_target == "x86_64-unknown-linux-gnu" {
-    println!(
-      "cargo:rustc-link-search={}",
-      "/usr/x86_64-unknown-linux-gnu/lib"
-    );
+    println!("cargo:rustc-link-search=/usr/x86_64-unknown-linux-gnu/lib");
   }
 }
