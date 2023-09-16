3f38ca52269f34582bb088ca34a4818aa9ab5c62
LongYinan <lynweklm@gmail.com>
2023-09-15 18:51:10 -0700

    build: fix missing libatomic on armv7 Linux

--- build.rs
+++ build.rs
@@ -3,7 +3,20 @@ extern crate napi_build;
 fn main() {
   napi_build::setup();
   let compile_target = std::env::var("TARGET").unwrap();
-  if compile_target == "x86_64-unknown-linux-gnu" {
-    println!("cargo:rustc-link-search=/usr/x86_64-unknown-linux-gnu/lib");
+  match compile_target.as_str() {
+    "x86_64-unknown-linux-gnu" => {
+      println!("cargo:rustc-link-search=/usr/x86_64-unknown-linux-gnu/lib");
+    }
+    "armv7-unknown-linux-gnueabihf" => {
+      const CROSS_LIB_PATH: &str = "/usr/lib/gcc-cross/arm-linux-gnueabihf";
+      if let Ok(version) = std::process::Command::new("ls")
+        .arg(CROSS_LIB_PATH)
+        .output()
+        .map(|o| String::from_utf8(o.stdout).unwrap().trim().to_string())
+      {
+        println!("cargo:rustc-link-search={CROSS_LIB_PATH}/{version}");
+      };
+    }
+    _ => {}
   }
 }
