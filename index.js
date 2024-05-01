e337df797d0e621bc9aef0f0b7d23a8eef0bc9f6
LongYinan <lynweklm@gmail.com>
2024-05-01 13:57:03 +0800

    chore: upgrade deps (#46)

--- index.js
+++ index.js
@@ -224,17 +224,32 @@ switch (platform) {
         }
         break
       case 'arm':
-        localFileExisted = existsSync(
-          join(__dirname, 'simple-git.linux-arm-gnueabihf.node')
-        )
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./simple-git.linux-arm-gnueabihf.node')
-          } else {
-            nativeBinding = require('@napi-rs/simple-git-linux-arm-gnueabihf')
+        if (isMusl()) {
+          localFileExisted = existsSync(
+            join(__dirname, 'simple-git.linux-arm-musleabihf.node')
+          )
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./simple-git.linux-arm-musleabihf.node')
+            } else {
+              nativeBinding = require('@napi-rs/simple-git-linux-arm-musleabihf')
+            }
+          } catch (e) {
+            loadError = e
           }
-        } catch (e) {
-          loadError = e
+        } else {
+          localFileExisted = existsSync(
+            join(__dirname, 'simple-git.linux-arm-gnueabihf.node')
+          )
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./simple-git.linux-arm-gnueabihf.node')
+            } else {
+              nativeBinding = require('@napi-rs/simple-git-linux-arm-gnueabihf')
+            }
+          } catch (e) {
+            loadError = e
+          }
         }
         break
       case 'riscv64':
